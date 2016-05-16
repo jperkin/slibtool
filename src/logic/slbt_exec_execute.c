e865d2e96a1bb12ef0e8bf18d516d34ae258816f
Jonathan Perkin <jperkin@joyent.com>
2016-05-16 14:20:58 +0100

    Ensure the wrapper can be found without "PATH=."

--- src/logic/slbt_exec_execute.c
+++ src/logic/slbt_exec_execute.c
@@ -45,7 +45,7 @@ int  slbt_exec_execute(
 	script  = ectx->cargv[1];
 
 	/* wrapper */
-	if ((size_t)snprintf(wrapper,sizeof(wrapper),"%s.exe.wrapper",
+	if ((size_t)snprintf(wrapper,sizeof(wrapper),"./%s.exe.wrapper",
 				script)
 			>= sizeof(wrapper)) {
 		slbt_free_exec_ctx(actx);
